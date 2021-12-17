b2cf9cd1711a1aab53eca36bb524d363d54a6c65
Peter Song <psong562@gmnail.com>
2021-12-16 20:05:18 -0800

    added inquirer function

--- index.js
+++ index.js
@@ -16,4 +16,89 @@
 const { promp } = require ('inquirer')
 const logo = require ('asciiart-logo')
 const db = require ('./db')
+const { initial } = require('lodash')
 require ('console.table')
+
+init() ;
+
+function  init() {
+    const logoText = logo ({name :'Employee Tracker'}).render();
+    console.log(logoText)
+    loadPrompt();
+}
+
+async function loadPromp() {
+  const { choice } = await prompt ([
+    {
+      type: 'list',
+      name: 'choice',
+      message: 'Please choose what you would like to do?',
+      choices :[
+        {
+          name: 'View All Employees',
+          value: 'VIEW_EMPLOYEES'
+        },
+        {
+          name: 'View All Employees by Department',
+          value: 'VIEW_EMPLOYEES_BY_DEPARTMENT'
+        },
+        {
+          name: 'View All Employees by MANAGER',
+          value: 'VIEW_EMPLOYEES_BY_MANAGER'
+        },
+        {
+          name: 'ADD Employee',
+          value: 'ADD_EMPLOYEE'
+        },
+        {
+          name: 'REMOVE Employee',
+          value: 'REMOVE_EMPLOYEE'
+        },
+        {
+          name: 'UPDATE Employee ROLE',
+          value: 'UPDATE_EMPLOYEE+ROLE'
+        },
+        {
+          name: 'UPDATE Employees MANAGER',
+          value: 'UPDATE_EMPLOYEE_MANAGER'
+        },
+        {
+          name: 'VIEW all roles',
+          value: 'VIEW_ROLES'
+        },
+        {
+          name: 'ADD roles',
+          value: 'ADD_ROLES'
+        },
+        {
+          name: 'REMOVE roles',
+          value: 'REMOVE_ROLES'
+        },
+        {
+          name: 'VIEW all departments',
+          value: 'VIEW_DEPARTMENTS'
+        },
+        {
+          name: 'ADD departments',
+          value: 'ADD_DEPARTMENTS'
+        },
+        {
+          name: 'REMOVE departments',
+          value: 'REMOVE_DEPARTMENTS'
+        },
+        {
+          name: 'VIEW all departments',
+          value: 'VIEW_DEPARTMENTS'
+        },
+        {
+          name: 'Quit',
+          value: 'Quit'
+        },
+      ]
+    }
+  ]);
+  
+// Using switch case call function on users choice
+
+
+}
